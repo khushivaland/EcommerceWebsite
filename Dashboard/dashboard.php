43a283712894d11232299e29eaf413ca96feedb1
khushi valand <khushivaland3286@gmail.com>
2023-04-21 13:14:30 +0530

    dashboard add

---
+++ Dashboard/dashboard.php
@@ -0,0 +1,75 @@
+<?php
+         $sql = "SELECT * FROM user ";
+         $query = mysqli_query($conn, $sql);
+         $result = mysqli_fetch_assoc($query);
+        
+       ?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="../Common/style.css"> -->
+    <title>Hello, world!</title>
+    <style>
+      .img{
+        background: url(banner.jpg) no-repeat;
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        width: 100%;
+}
+      
+    </style>
+  </head>
+  <body>
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Welcome!!</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">about</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <span>
+          <?php 
+           echo $result['name'];
+          ?>
+        </span>
+        <i class='bx bxs-user-circle'></i>
+      <li class="nav-btn">  
+      <a class="btn btn-outline-light" href="#">Log out</a>
+      </li>
+      </ul>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="img w-100 vh-100 d-flex justify-content-center align-items-center">
+  <div class="content text-center">
+    <h1>hello everyone</h1>
+  </div>
+</div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
